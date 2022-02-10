8ccf3431dc861588697f6fb811c72e17b969238a
Seconds <f10dev.it@gmail.com>
2022-02-10 13:05:25 +0700

    edit

---
+++ web/assest/header.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0">
+  <link rel="stylesheet" href=".../stylesheets/css/bootstrap.min.css">
+  <link rel="shortcut icon" href=".../stylesheets/js/bootstrap.bundle.min.js">
+<title>Seconds DEV</title>
+</head>
+ </html>
